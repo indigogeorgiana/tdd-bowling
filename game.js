83f3315edc4bd040d3acfbb832eddc4282e53cf4
Indigo Phillips <indigogeorgiana@gmail.com>
2018-05-31 11:13:22 +1200

    completed Single frames

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ module.exports = {
   scoreFrame: scoreFrame
 }
 
-function scoreFrame (frame) { // this frame currently equals either [0,0] or [2,3]   
+function scoreFrame (frame) { // this frame currently [[5, 5], [2, 3]]
+  if (frame[0][0] === 10) { 
+    return 10 + frame[1][0] + frame[1][1]
+  } else if (frame[0][0] + frame[0][1] === 10) {
+    return 10 + frame[1][0] 
+}
+  // if ball number 1, which is currently referenced by  ball number 2 which is referenced by  is  to  the score of a spare,
   return frame[0] + frame[1]
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -17,3 +17,17 @@ test('scores a normal frame', function () {
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
+test('scores a spare frame', function () {
+  const frame = [[5, 5], [2, 3]]
+  const expected = 12
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a single strike frame', function () {
+  const frame = [[10, 0], [2, 5]]
+  const expected = 17
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
