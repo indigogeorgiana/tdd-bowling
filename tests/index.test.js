e04a28186e1e8e44fca01a2bb4ca76dc18b7f844
Sam <sam-sanders@hotmail.co.uk>
2018-05-31 10:10:58 +1200

    Scoring gutterball frames

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,12 @@
+const game = require('../game') 
 
 test('test setup working', function () {
   expect(true).toBeTruthy()
+})
+
+test('scores a gutterball frame', function () {
+  const frame = [0, 0]
+  const expected = 0
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
 })
