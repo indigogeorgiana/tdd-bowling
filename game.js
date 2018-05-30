1970be4da244a47d2d1dc87d998116c9d23e923f
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-05-31 10:55:28 +1200

    test for strike made and passed

--- game.js
+++ game.js
@@ -1,7 +1,27 @@
 module.exports = {
-  scoreFrame
+  gutterFrame,
+  normalFrame,
+  spareFrame,
+  strikeFrame
 }
 
-function scoreFrame (frame) {
+function gutterFrame (frame) {
   return 0
 }
+
+function normalFrame (frame) {
+  return frame[0] + frame[1]
+}
+
+function spareFrame (frame) {
+  return frame[0][0] + frame[0][1] + frame[1][0]
+}
+
+function strikeFrame (frame) {
+  if (frame.length ===2){
+return frame[0][0] + frame[0][1] + frame[1][0] +frame[1][1]
+  } else {
+    return frame[0][0] + frame[0][1] + frame[1][0] +frame[1][1] + frame[2][0]
+  }
+}
+
--- tests/index.test.js
+++ tests/index.test.js
@@ -9,7 +9,37 @@ test('scores a gutterball frame', () => {
   const frame = [0, 0]
   const expected = 0
   // act
-  const actual = game.scoreFrame(frame)
+  const actual = game.gutterFrame(frame)
+  // assert
+  expect(actual).toBe(expected)
+})
+
+test('scores a normal frame', () => {
+  // arrange
+  const frame = [6, 2]
+  const expected = 8
+  // act
+  const actual = game.normalFrame(frame)
+  // assert
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare frame', () => {
+  // arrange
+  const frame = [[6, 4], [4]]
+  const expected = 14
+  // act
+  const actual = game.spareFrame(frame)
+  // assert
+  expect(actual).toBe(expected)
+})
+
+test('scores a strike frame', () => {
+  // arrange
+  const frame = [[10, 0], [10, 0], [5, 0]]
+  const expected = 25
+  // act
+  const actual = game.strikeFrame(frame)
   // assert
   expect(actual).toBe(expected)
 })
