cc50287fb9f2618f2f88d86a107ca6963f7cbbee
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-05-31 10:55:23 +1200

    updated scoreFrame to include single strike

--- game.js
+++ game.js
@@ -3,11 +3,17 @@ module.exports = {
 }
 
 function scoreFrame (frame) {
-  // if there is only one array of frames, simply add up the 
+  // if there is only one array of frames, simply add up the
   // two numbers in the array
   if (frame.length === 1) {
     return frame[0][0] + frame[0][1]
-  } 
+  }
+  // if it's a single strike we have two arrays of two nubmers and the
+  // first number of the first array is 10.
+  // add up all the numbers of both arrays
+  else if (frame[0][0] === 10) {
+    return (frame[0][0] + frame[0][1] + frame[1][0] + frame[1][1])
+  }
   // if it's a spare then we have two arrays of two numbers each
   // add up the numbers in the first array and add the first number
   // of the second array
--- tests/index.test.js
+++ tests/index.test.js
@@ -24,3 +24,17 @@ test('scores a spare frame', function () {
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
+test('scores a single strike frame', function () {
+  const frame = [[10, 0], [4, 5]]
+  const expected = 19
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a double strike frame', function () {
+  const frame = [[10, 0], [10, 0], [2,6]]
+  const expected = 19
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
