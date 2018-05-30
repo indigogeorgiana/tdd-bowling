0fe537f72a6bafbe1bd9acd1de0a1912086463ad
Caitlyn Bayley <caitlyn.bayley@gmail.com>
2018-05-31 10:19:06 +1200

    Scoring gutterball frames

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,12 @@
+const game = require('../game') // this is the line to add
+
 test('test setup working', function () {
   expect(true).toBeTruthy()
+})
+
+test('scores a gutterball frame', function () {
+  const frame = [0, 0]
+  const expected = 0
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
 })
