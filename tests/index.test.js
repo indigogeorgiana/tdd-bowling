2cd366a3df5ff8206af5e75a3f77c257f0043051
ZeBin Xiao <juniorxiao01@gmail.com>
2018-05-31 10:15:03 +1200

    add in spare function

--- tests/index.test.js
+++ tests/index.test.js
@@ -18,3 +18,10 @@ test('scores a normal frame', function () {
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
+
+test('scores a spare frame', function () {
+  const frame = [[7, 3], [3, 4]]
+  const expected = 13
+  const actual = game.spare(frame)
+  expect(actual).toBe(expected)
+})
