6a090494630a601ecbaa5f43c6106d7425c0d8e4
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-05-31 10:24:22 +1200

    made test for gutterball and passed

---
+++ game.js
@@ -0,0 +1,7 @@
+module.exports = {
+  scoreFrame
+}
+
+function scoreFrame (frame) {
+  return 0
+}
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,15 @@
+const game = require('../game')
+
 test('test setup working', () => {
   expect(true).toBeTruthy()
-} )
+})
+
+test('scores a gutterball frame', () => {
+  // arrange
+  const frame = [0, 0]
+  const expected = 0
+  // act
+  const actual = game.scoreFrame(frame)
+  // assert
+  expect(actual).toBe(expected)
+})
