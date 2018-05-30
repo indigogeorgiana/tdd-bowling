4cc081ac1fc51155c04f0a99871856b20c87ff52
Cornelia Schulz <cornelia.schulz.nz@gmail.com>
2018-05-31 10:39:58 +1200

    updated score function to include spare calculation

--- game.js
+++ game.js
@@ -3,5 +3,15 @@ module.exports = {
 }
 
 function scoreFrame (frame) {
-  return frame[0] + frame[1]
+  // if there is only one array of frames, simply add up the 
+  // two numbers in the array
+  if (frame.length === 1) {
+    return frame[0][0] + frame[0][1]
+  } 
+  // if it's a spare then we have two arrays of two numbers each
+  // add up the numbers in the first array and add the first number
+  // of the second array
+  else if (frame[0][0] + frame[0][1] === 10) {
+    return (frame[0][0] + frame[0][1] + frame[1][0])
+  }
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,19 +1,26 @@
-const game = require('../game') 
+const game = require('../game')
 
 test('test setup working', function () {
   expect(true).toBeTruthy()
 })
 
 test('scores a gutterball frame', function () {
-  const frame = [0, 0]
+  const frame = [[0, 0]]
   const expected = 0
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
 
 test('scores a normal frame', function () {
-  const frame = [2, 3]
+  const frame = [[2, 3]]
   const expected = 5
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a spare frame', function () {
+  const frame = [[2, 8], [4, 5]]
+  const expected = 14
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
