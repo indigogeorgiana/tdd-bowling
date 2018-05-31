30d008b5220c0c35a826b40b469700427e0b445c
Indigo Phillips <indigogeorgiana@gmail.com>
2018-05-31 14:24:11 +1200

    started on iterating through game

--- game.js
+++ game.js
@@ -2,12 +2,22 @@ module.exports = {
   scoreFrame: scoreFrame
 }
 
-function scoreFrame (frame) { // this frame currently [[5, 5], [2, 3]]
-  if (frame[0][0] === 10) { 
-    return 10 + frame[1][0] + frame[1][1]
-  } else if (frame[0][0] + frame[0][1] === 10) {
-    return 10 + frame[1][0] 
-}
-  // if ball number 1, which is currently referenced by  ball number 2 which is referenced by  is  to  the score of a spare,
-  return frame[0] + frame[1]
+function scoreFrame (frame) { // this frame currently [[7, 0], [10, 0], [2, 5]]
+  // if statement for totaling a double strike
+  (var i = 0; i  < 9 ; i++){
+    for (i = )
+    if (frame[0][0] === 10 && frame[1][0] === 10) {
+      return 20 + frame[2][0]
+    } 
+    // If statement for totaling a single strike
+    else if (frame[0][0] === 10) {
+      return 10 + frame[1][0] + frame[1][1]
+    }
+    // If statement for totaling a spare
+    else if (frame[0][0] + frame[0][1] === 10) {
+      return 10 + frame[1][0]
+    }
+    // if ball number 1, which is currently referenced by  ball number 2 which is referenced by  is  to  the score of a spare,
+    return frame[0][0] + frame[0][1]  
+  }
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -5,14 +5,14 @@ test('test setup working', function () {
 })
 
 test('scores a gutterball frame', function () {
-  const frame = [0, 0]
+  const frame = [[0, 0]]
   const expected = 0
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
 
 test('scores a normal frame', function () {
-  const frame = [2, 3]
+  const frame = [[2, 3]]
   const expected = 5
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
@@ -28,6 +28,20 @@ test('scores a spare frame', function () {
 test('scores a single strike frame', function () {
   const frame = [[10, 0], [2, 5]]
   const expected = 17
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a double strike frame', function () {
+  const frame = [[10, 0], [10, 0], [2, 5]]
+  const expected = 22
+  const actual = game.scoreFrame(frame)
+  expect(actual).toBe(expected)
+})
+
+test('scores a game', function () {
+  const frame = [[1, 2], [6, 4], [5, 4], [10, 0], [7, 2], [10, 0], [10, 0], [5, 2], [7, 0], [4, 4]]
+  const expected = 111
   const actual = game.scoreFrame(frame)
   expect(actual).toBe(expected)
 })
